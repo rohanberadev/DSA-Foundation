739a51e01062d8bf8cda7a3fc8bfb892a184d54e
Rohan Bera <104846134+hotliquid@users.noreply.github.com>
2024-07-06 14:31:55 +0530

    Linked List 2

--- 5 Linked List/2_Medium_Problems_of_LL/find_middle_LL.go
+++ 5 Linked List/2_Medium_Problems_of_LL/find_middle_LL.go
@@ -2,6 +2,10 @@ package mediumproblemsofll
 
 import ll "linked_list/1_DS/Singly_Linked_List"
 
+/*
+Problem Statement: Given the head of a linked list of integers, determine the middle node of the linked list. However, if the linked list has an even number of nodes, return the second middle node.
+*/
+
 // Time :- O(N + N/2)
 func FindMiddleLL_Brute(ls *ll.LinkedList) *ll.Node {
 	// Time :- O(N)
