70b34456e06f027501cf9cab2caa1391277540b0
rohanberadev <rohanbera4002@gmail.com>
2024-08-23 20:23:43 +0530

    Commit

--- 06_Recursion/1_Strong_Hold/4_Sort_a_stack/main.cpp
+++ 06_Recursion/1_Strong_Hold/4_Sort_a_stack/main.cpp
@@ -2,44 +2,27 @@
 using namespace std;
 
 void insertAtCorrectPos(stack<int> &st, int data) {
-	if (st.empty() || st.top() <= data) {
-		st.push(data);
-		return;
-	}
+  if (st.empty() || st.top() <= data) {
+    st.push(data);
+    return;
+  }
 
-	int x = st.top();
-	st.pop();
-	insertAtCorrectPos(st, data);
-	st.push(x);
+  int x = st.top();
+  st.pop();
+  insertAtCorrectPos(st, data);
+  st.push(x);
 }
 
 // Time :- O(N^2)
 void sortStack(stack<int> &st) {
-	if (st.empty()) return;
+  if (st.empty())
+    return;
 
-	int x = st.top();
-	st.pop();
-	sortStack(st);
-	insertAtCorrectPos(st, x);
+  int x = st.top();
+  st.pop();
+  sortStack(st);
+  insertAtCorrectPos(st, x);
 }
 
-int main() {
-	int size;
-	cin >> size;
-
-	stack<int> st;
-	for (int i = 0; i < size; i++) {
-		int data;
-		cin >> data;
-		st.push(data);
-	}
-
-	sortStack(st);
-
-	while (!st.empty()) {
-		cout << st.top() << " ";
-		st.pop();
-	}
-
-	return 0;
-}
+// Link -
+// https://bit.ly/3Pu0YBn
--- 06_Recursion/1_Strong_Hold/5_Reverse_a_stack/main.cpp
+++ 06_Recursion/1_Strong_Hold/5_Reverse_a_stack/main.cpp
@@ -2,43 +2,26 @@
 using namespace std;
 
 void insertAtBottom(stack<int> &st, int data) {
-	if (st.empty()) {
-		st.push(data);
-		return;
-	}
+  if (st.empty()) {
+    st.push(data);
+    return;
+  }
 
-	int x = st.top();
-	st.pop();
-	insertAtBottom(st, data);
-	st.push(x);
+  int x = st.top();
+  st.pop();
+  insertAtBottom(st, data);
+  st.push(x);
 }
 
 void reverseStack(stack<int> &st) {
-	if (st.empty()) return;
+  if (st.empty())
+    return;
 
-	int x = st.top();
-	st.pop();
-	reverseStack(st);
-	insertAtBottom(st, x);
+  int x = st.top();
+  st.pop();
+  reverseStack(st);
+  insertAtBottom(st, x);
 }
 
-int main() {
-	int size;
-	cin >> size;
-
-	stack<int> st;
-	for (int i = 0; i < size; i++) {
-		int data;
-		cin >> data;
-		st.push(data);
-	}
-
-	reverseStack(st);
-
-	while (!st.empty()) {
-		cout << st.top() << " ";
-		st.pop();
-	}
-
-	return 0;
-}
+// Link -
+// https://bit.ly/3podAiY
