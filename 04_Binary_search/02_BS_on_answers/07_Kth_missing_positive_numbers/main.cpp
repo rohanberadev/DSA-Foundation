edeefe0ddf9cf8b2b53480a2c9c93947bcac5bc6
rohanberadev <rohanbera4002@gmail.com>
2024-08-23 19:43:34 +0530

    Commit

--- 04_Binary_search/02_BS_on_answers/07_Kth_missing_positive_numbers/main.cpp
+++ 04_Binary_search/02_BS_on_answers/07_Kth_missing_positive_numbers/main.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int findKthPositive(vector<int> &arr, int k) {}
+
 // Link -
 // https://leetcode.com/problems/kth-missing-positive-number/
--- 06_Recursion/1_Strong_Hold/1_Recursive_Atoi/Iterative/main.cpp
+++ 06_Recursion/1_Strong_Hold/1_Recursive_Atoi/Iterative/main.cpp
@@ -1,52 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool checkNum(char c) {
-	return c >= '0' && c <= '9';
-}
+bool checkNum(char c) { return c >= '0' && c <= '9'; }
 
 int atoiIterative(string s, int n) {
-	int ind = 0;
-	while (ind < n && s[ind] == ' ') {
-		ind++;
-	}
+  int ind = 0;
+  while (ind < n && s[ind] == ' ') {
+    ind++;
+  }
 
-	if (ind == n) {
-		return 0;
-	}	
+  if (ind == n) {
+    return 0;
+  }
 
-	int sign = +1;
-	int MAX = INT_MAX;
-	int MIN = INT_MIN;
-	int num = 0;
+  long sign = +1;
+  int MAX = INT_MAX;
+  int MIN = INT_MIN;
+  long num = 0;
 
-	if (s[ind] == '-') {sign = -1; ind++;}
-	else if (s[ind] == '+') {sign = +1; ind++;}
+  if (s[ind] == '-') {
+    sign = -1;
+    ind++;
+  } else if (s[ind] == '+') {
+    sign = +1;
+    ind++;
+  }
 
-	while (ind < n) {
-		if (!checkNum(s[ind])) break;
+  while (ind < n) {
+    if (!checkNum(s[ind]))
+      break;
 
-		num *= 10;
-		num += int(s[ind] - '0');
+    num *= 10;
+    num += int(s[ind] - '0');
 
-		if (num*sign < MIN) return MIN;
-		else if (num*sign > MAX) return MAX;
+    if (num * sign < MIN)
+      return MIN;
+    else if (num * sign > MAX)
+      return MAX;
 
-		ind++;
-	}
+    ind++;
+  }
 
-	return num*sign;
+  return num * sign;
 }
 
-int myAtoi(string s) {
-	return atoiIterative(s, s.size());
-}
+int myAtoi(string s) { return atoiIterative(s, s.size()); }
 
-int main() {
-	string s;
-	cin >> s;
-
-	cout << myAtoi(s);
-
-	return 0;
-}
+// Link -
+// https://leetcode.com/problems/string-to-integer-atoi/
